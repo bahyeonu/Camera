55e6b513f936cd24896aa4d88746dd9666c4efc5
bahyeonu <bahyeonu@naver.com>
2020-03-03 17:46:02 +0900

    commit

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,40 +1,36 @@
 package com.example.camera
 
-import android.graphics.SurfaceTexture
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.TextureView
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textureView: TextureView = findViewById(R.id.textureView)
+        val textureView: TextureView =  findViewById(R.id.textureView)
         val btnTakePicture: Button = findViewById(R.id.btnTakePicture)
         val btnLoadPicture: Button = findViewById(R.id.btnLoadPicture)
 
+        btnTakePicture.setOnClickListener(){
+            dispatchTakePictureIntent()
+        }
+
     }
 
-    private var textureListener: TextureView.SurfaceTextureListener = object : TextureView.SurfaceTextureListener{
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
-
-        }
-
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
-
-        }
-
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
-            return false
-        }
-
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-            openCamera()
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
     }
-
 
 }
